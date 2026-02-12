a4fa0c175ec5c1310d002c7e2d584bd550bbc538
Ramon Junior Urena Vasquez <ramon_urena2@ucne.edu.do>
2026-02-11 21:19:55 -0400

    Add project files.

---
+++ Los4Carnales/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Los4Carnales.Models;
+
+public class Categoria
+{
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [Required]
+    public string Nombre { get; set; }
+
+}
---
+++ Los4Carnales/Models/Proveedor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Los4Carnales.Models;
+
+public class Proveedor
+{
+    [Key]
+    public int ProveedorId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "El teléfono es obligatorio.")]
+    [RegularExpression(@"^(809|829|849)-\d{3}-\d{4}$",
+        ErrorMessage = "El teléfono debe tener el formato 809-000-0000 y comenzar con 809, 829 o 849.")]
+    public string Telefono { get; set; }
+
+    [ForeignKey("CategoriaId")]
+    public ICollection<Categoria> ProveedorDetalle { get; set; } = new List<Categoria>();
+}
+
